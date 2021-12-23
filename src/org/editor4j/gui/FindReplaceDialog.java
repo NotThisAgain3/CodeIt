aeb290e00aa89cd1acbdc251da520c96252011e2
Mohammed <skyloftmb@gmail.com>
2021-12-23 12:08:59 +0530

    Rewrote Find And Replace

--- src/org/editor4j/gui/FindReplaceDialog.java
+++ src/org/editor4j/gui/FindReplaceDialog.java
@@ -3,107 +3,99 @@ package org.editor4j.gui;
 import org.editor4j.Editor;
 import org.editor4j.gui.components.JBaseDialog;
 import org.editor4j.gui.components.JField;
-import org.editor4j.managers.FileManager;
 import org.fife.ui.rtextarea.SearchContext;
 import org.fife.ui.rtextarea.SearchEngine;
-import org.fife.ui.rtextarea.SearchResult;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class FindReplaceDialog extends JBaseDialog {
     JPanel jPanel = new JPanel();
-    JPanel optionsPanel = new JPanel();
-    JButton doFindOrReplace = new JButton("Find");
-    JTextField find = new JTextField();
-    JTextField replaceWith = new JTextField();
 
-    JRadioButton up = new JRadioButton("Up (from cursor)");
-    JRadioButton down = new JRadioButton("Down (from cursor)");
-    JCheckBox matchCase = new JCheckBox("Match Case");
-    JCheckBox replace = new JCheckBox("Find and Replace");
+
+    JTextField textToFind = new JTextField();
+    JTextField replacementText = new JTextField();
+    JField replacementTextField = new JField("Replace With", replacementText);
+    JPanel textFieldsPanel = new JPanel();
+    JCheckBox doFindAndReplace = new JCheckBox();
+    JButton jButton = new JButton("Find");
+
+    JRadioButton backward = new JRadioButton(), forward = new JRadioButton();
+    JCheckBox matchCase = new JCheckBox();
+
+
     public FindReplaceDialog(Editor editor) {
-        super("Find/Replace", 580, 200);
+        super("Find/Replace", 320, 240);
 
         jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
-        jPanel.add(new JField("Look For", find));
-        jPanel.add(new JField("Replace With", replaceWith));
+
+        textFieldsPanel.setLayout(new BoxLayout(textFieldsPanel, BoxLayout.Y_AXIS));
+        textFieldsPanel.add(new JField("Find", textToFind));
 
 
+        jPanel.add(textFieldsPanel);
 
+        doFindAndReplace.addActionListener(e -> {
+            if(doFindAndReplace.isSelected()) {
+                textFieldsPanel.add(replacementTextField);
+                jButton.setText("Replace");
+            }
+            else {
+                textFieldsPanel.remove(replacementTextField);
+                jButton.setText("Find");
+            }
 
-        ButtonGroup buttonGroup = new ButtonGroup();
-        buttonGroup.add(up);
-        buttonGroup.add(down);
+            textFieldsPanel.revalidate();
+            textFieldsPanel.repaint();
 
+        });
+
+        JPanel optionsPanel = new JPanel();
         optionsPanel.setLayout(new FlowLayout());
-        optionsPanel.add(up);
-        optionsPanel.add(down);
-        optionsPanel.add(matchCase);
-        optionsPanel.add(replace);
+        optionsPanel.add(new JField("Find And Replace", doFindAndReplace));
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(backward);
+        buttonGroup.add(forward);
+        optionsPanel.add(new JField("Backward (Up)", backward));
+        optionsPanel.add(new JField("Forward (Down)", forward));
+        optionsPanel.add(new JField("Match Case", matchCase));
 
-        replace.addActionListener(e -> {
-            if(replace.isSelected()) {
-                replaceWith.setEnabled(true);
-                doFindOrReplace.setText("Replace");
-            }
-            else {
-                doFindOrReplace.setText("Find");
-                replaceWith.setEnabled(false);
-            }
-        });
-
-        down.setSelected(true);
         jPanel.add(optionsPanel);
-
-
-
-        doFindOrReplace.addActionListener(e -> {
-            boolean searchDown;
-
-            searchDown = !up.isSelected();
-
-            SearchContext context = new SearchContext();
-
-            String text = find.getText();
-            if (text.length() == 0)
-                return;
-
-            context.setSearchFor(text);
-
-
-
-            context.setMatchCase(matchCase.isSelected());
-
-            context.setRegularExpression(false);
-            //Here down is forward and up is backward
-            context.setSearchForward(searchDown);
-            context.setWholeWord(false);
-
-            SearchResult searchResult;
-            if(replace.isSelected()) {
-                context.setReplaceWith(replaceWith.getText());
-                searchResult = SearchEngine.replace(editor.codeEditor, context);
-            }
-            else {
-                searchResult = SearchEngine.find(editor.codeEditor, context);
-            }
-
-
-
-            if (!searchResult.wasFound()) {
-                String searchDirection = searchDown ? "down" : "up";
-                JOptionPane.showMessageDialog(this, "No more matches found further " + searchDirection);
-            }
-
-
-        });
-        if(FileManager.openedFile == null)
-            doFindOrReplace.setEnabled(false);
         setModalityType(ModalityType.APPLICATION_MODAL);
 
+
+
+
+        jButton.addActionListener(e -> {
+
+            boolean fr = doFindAndReplace.isSelected();
+
+            SearchContext context = new SearchContext();
+            String text = textToFind.getText();
+            if (text.length() == 0) {
+                return;
+            }
+            context.setSearchFor(text);
+            context.setMatchCase(matchCase.isSelected());
+            context.setSearchForward(forward.isSelected());
+            context.setWholeWord(false);
+
+            if(fr)
+                context.setReplaceWith(replacementText.getText());
+
+            if(fr)
+                SearchEngine.replace(editor.codeEditor, context);
+            else
+                SearchEngine.find(editor.codeEditor, context).wasFound();
+
+        });
+
+
+
+        super.setDefaultButtonOnly(jButton);
         super.setContent(jPanel);
-        super.setDefaultButtonOnly(doFindOrReplace);
+
+
     }
 
 
